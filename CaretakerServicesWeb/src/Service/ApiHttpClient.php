145c2ed296efca58013178854392e4c831726d8d
LumineWollah <mathis.vareilles@yahoo.com>
2024-04-06 21:37:21 +0200

    fix error in APIHttpClient

--- CaretakerServicesWeb/src/Service/ApiHttpClient.php
+++ CaretakerServicesWeb/src/Service/ApiHttpClient.php
@@ -21,4 +21,15 @@ class ApiHttpClient
             'headers' => $headers
         ]);
     }
+
+    public function getClientWithoutBearer()
+    {
+        return HttpClient::create([
+            'base_uri' => 'http://127.0.0.1:8000/api/',
+            'headers' => [
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json'
+            ]
+        ]);
+    }
 }
