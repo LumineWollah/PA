5a23bcd209f92d9ed128986f3c230e49288e48ca
Léopold Goudier <leopold.goudier@gmail.com>
2024-04-11 17:31:04 +0200

    add fixtures

--- CaretakerServicesApi/src/DataFixtures/AppFixtures.php
+++ CaretakerServicesApi/src/DataFixtures/AppFixtures.php
@@ -35,23 +35,27 @@ class AppFixtures extends Fixture
         $user->setRoles(['ROLE_LESSOR']);
         $manager->persist($user);
 
-        $apartment = new CsApartment();
-        $apartment->setName("Mon super appart");
-        $apartment->setDescription("Test de description");
-        $apartment->setBedrooms(4);
-        $apartment->setTravelersMax(8);
-        $apartment->setArea(825.25);
-        $apartment->setIsFullhouse(true);
-        $apartment->setIsHouse(true);
-        $apartment->setPrice(421);
-        $apartment->setOwner($user);
-        $apartment->setAddress("71, avenue d'Italie");
-        $apartment->setPostalCode("75013");
-        $apartment->setCity("Paris");
-        $apartment->setCountry("France");
-        $apartment->setMainPict("https://tinyurl.com/mr2umuh3");
-        $apartment->setPictures(["https://tinyurl.com/2w8fnhrs", "https://tinyurl.com/4bck47tz"]);
-        $manager->persist($apartment);
+        $images = ["https://tinyurl.com/ycyr8zdf", "https://tinyurl.com/9mmcfchx", "https://tinyurl.com/2ss5cam3", "https://tinyurl.com/6s9cpzhz", "https://tinyurl.com/bdeaxv7d", "https://tinyurl.com/mrxwuyy3", "https://tinyurl.com/mrxcrxfr", "https://tinyurl.com/5t4vwx47", "https://tinyurl.com/5bhk7a8f"];
+
+        for ($i=0; $i < 32; $i++) { 
+            $apartment = new CsApartment();
+            $apartment->setName("Mon super appart ".$i);
+            $apartment->setDescription("Contemplez le coucher de soleil sur les flots depuis la terrasse de cet appartement récemment rénové. Confortable et parfaitement aménagé, il possède une décoration soignée et des téléviseurs à écran plat dans chacune des deux chambres.\n\nDans le cadre de la pandémie de coronavirus (COVID-19), nous appliquons actuellement des mesures sanitaires supplémentaires.\n\nD'une superficie de 45 m2, l'appartement a été refait à neuf en 2018. J'ai choisi des matériaux et du mobilier \"comme si c'était pour moi\" : 2 chambres avec chacune sa petite salle d'eau, T.V écran plat au mur dans chaque chambre , séjour avec petite terrasse et vue mer magnifique, cuisine équipée d'un lave vaisselle, réfrigérateur/compartiment congélateur, four micro-onde et traditionnel, machine à laver le linge séchante, machine Nespresso et tout ce qu'il faut pour cuisiner :-)");
+            $apartment->setBedrooms(4);
+            $apartment->setTravelersMax(8);
+            $apartment->setArea(825.25);
+            $apartment->setIsFullhouse(true);
+            $apartment->setIsHouse(true);
+            $apartment->setPrice(421);
+            $apartment->setOwner($user);
+            $apartment->setAddress("71, avenue d'Italie");
+            $apartment->setPostalCode("75013");
+            $apartment->setCity("Paris");
+            $apartment->setCountry("France");
+            $apartment->setMainPict($images[rand(0, 8)]);
+            $apartment->setPictures(["https://tinyurl.com/2w8fnhrs", "https://tinyurl.com/4bck47tz"]);
+            $manager->persist($apartment);
+        }
 
         $company = new CsCompany();
         $company->setCompanyName("Boite de glands");
