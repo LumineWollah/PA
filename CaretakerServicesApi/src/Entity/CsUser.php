8a811c9f4d930f004970d2d9c8d3293da42212fc
LumineWollah <mathis.vareilles@yahoo.com>
2024-04-16 20:49:03 +0200

    bug fix (the boss is a twat)

--- CaretakerServicesApi/src/Entity/CsUser.php
+++ CaretakerServicesApi/src/Entity/CsUser.php
@@ -40,21 +40,21 @@ class CsUser implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies"])]
+    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies", "getServices"])]
     private ?int $id = null;
 
     #[ORM\Column(length: 180)]
-    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies"])]
+    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies", "getServices"])]
     private ?string $email = null;
 
     #[ORM\Column(length: 150)]
-    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies"])]
+    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies", "getServices"])]
     #[Assert\NotBlank(message: "Le prénom est obligaoire")]
     #[Assert\Length(min: 3, max: 150, minMessage: "Le prénom doit faire au moins {{ limit }} caractères", maxMessage: "Le prénom ne peut pas faire plus de {{ limit }} caractères")]
     private ?string $firstname = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies"])]
+    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies", "getServices"])]
     private ?string $lastname = null;
 
     /**
@@ -64,35 +64,35 @@ class CsUser implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $password = null;
 
     #[ORM\Column]
-    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies"])]
+    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies", "getServices"])]
     private ?DateTime $lastConnection = null;
 
     #[ORM\Column]
-    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies"])]
+    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies", "getServices"])]
     private ?DateTime $dateInscription = null;
 
     #[ORM\Column(length: 500, nullable: true)]
-    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies"])]
+    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies", "getServices"])]
     private ?string $profilePict = null;
 
     #[ORM\Column(type: 'json')]
-    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies"])]
+    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies", "getServices"])]
     private $roles = [];
 
     #[ORM\Column(length: 10)]
-    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies"])]
+    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies", "getServices"])]
     private ?string $telNumber = null;
 
     #[ORM\Column]
-    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies"])]
+    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies", "getServices"])]
     private ?bool $isVerified = false;
 
     #[ORM\Column]
-    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies"])]
+    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies", "getServices"])]
     private ?bool $professional = false;
 
     #[ORM\Column(nullable: true)]
-    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies"])]
+    #[Groups(["getUsers", "getDocuments", "getApartments", "getCompanies", "getServices"])]
     private ?int $subscription = null;
 
     #[ORM\OneToMany(targetEntity: CsDocument::class, mappedBy: 'owner', orphanRemoval: true)]
