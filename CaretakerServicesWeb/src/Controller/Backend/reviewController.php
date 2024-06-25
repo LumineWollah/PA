a1737ed2851bf10cff5cd530c2b1e1aaf410e0ea
LumineWollah <mathis.vareilles@yahoo.com>
2024-06-25 20:55:42 +0200

    Lol

--- CaretakerServicesWeb/src/Controller/Backend/reviewController.php
+++ CaretakerServicesWeb/src/Controller/Backend/reviewController.php
@@ -20,7 +20,7 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\File;
 
-class reviewController extends AbstractController
+class reviewsController extends AbstractController
 {
     private $apiHttpClient;
     private $amazonS3Client;
